b698e6f2aedc7884e6b52878e4584a4ae7c754bc
moonj <moonjihawn@naver.com>
2024-02-12 01:50:08 +0900

    상품등록 업데이트

--- src/product/domain/Brand.java
+++ src/product/domain/Brand.java
@@ -6,5 +6,23 @@ public class Brand {
     // 브랜드 이름
     private String name;
     // 브랜드 코드
-    private String code;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
 }
--- src/product/domain/Products.java
+++ src/product/domain/Products.java
@@ -1,16 +1,101 @@
 package product.domain;
 
 public class Products {
-    // 상품 id
-    private Long id;
-    // 상품 이름
-    private String name;
-    // 판매가
-    private int price;
-    // 원가
-    private int cost;
-    // 사업자 id
-    private Long business_id;
-    // 브랜드 id
-    private Long brand_id;
+    private int id;
+    private boolean status;
+    private Integer cost;
+    private int quantity;
+    private Integer price;
+    private Integer brandCodeId;
+    private int ownerId;
+    private int warehouseId;
+
+    public Products(int id, boolean status, Integer cost, int quantity, Integer price, Integer brandCodeId, int ownerId, int warehouseId) {
+        this.id = id;
+        this.status = status;
+        this.cost = cost;
+        this.quantity = quantity;
+        this.price = price;
+        this.brandCodeId = brandCodeId;
+        this.ownerId = ownerId;
+        this.warehouseId = warehouseId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getBrandCodeId() {
+        return brandCodeId;
+    }
+
+    public void setBrandCodeId(Integer brandCodeId) {
+        this.brandCodeId = brandCodeId;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getWarehouseId() {
+        return warehouseId;
+    }
+
+    public void setWarehouseId(int warehouseId) {
+        this.warehouseId = warehouseId;
+    }
+
+    @Override
+    public String toString() {
+        return "Products{" +
+                "id=" + id +
+                ", status=" + status +
+                ", cost=" + cost +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", brandCodeId=" + brandCodeId +
+                ", ownerId=" + ownerId +
+                ", warehouseId=" + warehouseId +
+                '}';
+    }
 }
