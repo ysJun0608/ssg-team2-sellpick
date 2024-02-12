53e65617899df0be938833b894394cd5f01fcebd
moonj <moonjihawn@naver.com>
2024-02-12 14:25:07 +0900

    test

--- src/product/dao/BrandDao.java
+++ src/product/dao/BrandDao.java
@@ -1,9 +1,70 @@
 package product.dao;
 
-import DBIO.ObjectDBIO;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.sql.Connection;
+public class BrandDao {
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/sqldb?serverTimezone=Asia/Seoul";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "admin1234";
 
-public class BrandDao extends ObjectDBIO {
-    Connection conn = null;
+    public String getBrandName(int brandCodeId) {
+        Connection conn = getConnection();
+        String brandName = null;
+
+        try {
+            String sql = "SELECT name FROM brand WHERE id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, brandCodeId);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                brandName = rs.getString("name");
+            }
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return brandName;
+    }
+
+    public List<String> getAllBrands() {
+        Connection conn = getConnection();
+        List<String> brand = new ArrayList<>();
+
+        try {
+            String sql = "SELECT name FROM brand";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                brand.add(rs.getString("name"));
+            }
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return brand;
+    }
+
+    private Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
