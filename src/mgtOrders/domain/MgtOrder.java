410324e5da9836edbae61fa5c5dc6c345fe908de
bjh <ekmbjh@naver.com>
2024-02-12 15:19:26 +0900

    feat : 발주등록(단건) - 확정, 확정 취소 기능 추가 refactor : 신규등록 기능에서 psmt 실행문 추가

--- src/mgtOrders/domain/MgtOrder.java
+++ src/mgtOrders/domain/MgtOrder.java
@@ -18,8 +18,19 @@ public class MgtOrder {
     // 매입 거래처
     private String purchaser;
 
+//    // 창고 id
+//    private Long warehouseId;
+
     // 상품 id
     private Long productsId;
+
+
+
+//    // 발주 구분
+//    private String orderType;
+//
+//    // 발주 유형
+//    private String orderCategory;
 
     // 수량
     private int quantity;
@@ -27,12 +38,5 @@ public class MgtOrder {
     // 확정여부
     private boolean confirm;
 
-//    // 창고 id
-//    private Long warehouseId;
-//
-//    // 발주 구분
-//    private String orderType;
-//
-//    // 발주 유형
-//    private String orderCategory;
+
 }
