0a261713ece9a0234b689fb6c32482c481f42cb7
jjuuuunn <didtdjwns111@gmail.com>
2024-02-12 12:31:20 +0900

    REFACTOR : Domain Field erd 변경으로 domain 필드도 변경

--- src/delivery/domain/DeliveryCmp.java
+++ src/delivery/domain/DeliveryCmp.java
@@ -1,13 +1,27 @@
 package delivery.domain;
 
-    // 더미 데이터 만들어서 사용
+// 더미 데이터 만들어서 사용
 public class DeliveryCmp {
     // 택배사 id
     private Long id;
     // 택배사 이름
     private String name;
-    // 택배사 코드
-    private String code;
 
     // GETTER, SETTER
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
