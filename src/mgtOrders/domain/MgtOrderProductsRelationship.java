f4564d77a889004570ac21a30484bed6b781ce0a
bjh <ekmbjh@naver.com>
2024-02-13 12:50:18 +0900

    ERD 수정에 따른 Class 생성

---
+++ src/mgtOrders/domain/MgtOrderProductsRelationship.java
@@ -0,0 +1,15 @@
+package mgtOrders.domain;
+
+public class MgtOrderProductsRelationship {
+    // 발주 상품 관계 id
+    private int id;
+
+    // 수량
+    private int quantity;
+
+    // 상품 id
+    private int productsId;
+
+    // 발주 id
+    private int mgtOrdersId;
+}
