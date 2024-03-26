8b2558440bee978d6d6086bb7613e64a8050e641
jjuuuunn <didtdjwns111@gmail.com>
2024-03-26 12:22:17 +0900

    merge

--- src/main/java/com/ssg/wsmt/member/controller/UserController.java
+++ src/main/java/com/ssg/wsmt/member/controller/UserController.java
@@ -69,36 +69,36 @@ public class UserController {
         return "redirect:/login/login";
     }
 
-    @PostMapping("/loginProc")
-    public String loginProc(HttpServletRequest request, HttpServletResponse response) {
-        // 사용자가 제출한 로그인 폼에서 입력한 이메일과 비밀번호 가져오기
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-
-        // 사용자를 나타내는 UserDetails 객체 생성
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
-
-        try {
-            // 사용자 인증을 위한 UsernamePasswordAuthenticationToken 생성
-            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
-
-            // AuthenticationManager를 사용하여 사용자 인증 수행
-            Authentication authentication = authenticationManager.authenticate(authToken);
-
-            // 인증이 성공하면 SecurityContextHolder에 인증 정보 저장
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            // 세션에 사용자 정보 저장
-            HttpSession session = request.getSession();
-            session.setAttribute("user", userDetails);
-
-            // 홈 페이지로 리다이렉트
-            return "redirect:/";
-        } catch (AuthenticationException e) {
-            // 인증 실패 시 에러 메시지를 파라미터로 전달하여 로그인 페이지로 리다이렉트
-            return "redirect:/login/login?error=true";
-        }
-    }
+//    @PostMapping("/loginProc")
+//    public String loginProc(HttpServletRequest request, HttpServletResponse response) {
+//        // 사용자가 제출한 로그인 폼에서 입력한 이메일과 비밀번호 가져오기
+//        String email = request.getParameter("email");
+//        String password = request.getParameter("password");
+//
+//        // 사용자를 나타내는 UserDetails 객체 생성
+//        UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+//
+//        try {
+//            // 사용자 인증을 위한 UsernamePasswordAuthenticationToken 생성
+//            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+//
+//            // AuthenticationManager를 사용하여 사용자 인증 수행
+//            Authentication authentication = authenticationManager.authenticate(authToken);
+//
+//            // 인증이 성공하면 SecurityContextHolder에 인증 정보 저장
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//
+//            // 세션에 사용자 정보 저장
+//            HttpSession session = request.getSession();
+//            session.setAttribute("user", userDetails);
+//
+//            // 홈 페이지로 리다이렉트
+//            return "redirect:/";
+//        } catch (AuthenticationException e) {
+//            // 인증 실패 시 에러 메시지를 파라미터로 전달하여 로그인 페이지로 리다이렉트
+//            return "redirect:/login/login?error=true";
+//        }
+//    }
 
 }
 
